a2de255f7e907e2092d429044c6921c81b4403ca
limegch <metin.gemil@gmail.com>
2021-01-12 12:23:25 +0200

    Added font2 patch. Adjusted fonts for colored emoji support

--- config.def.h
+++ config.def.h
@@ -6,6 +6,14 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"JoyPixels:pixelsize=7:antialias=true:autohint=true",
+//	"Noto Color Emoji:pixelsize=9:antialias=true:autohint=true",
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
 static int borderpx = 2;
 
 /*
